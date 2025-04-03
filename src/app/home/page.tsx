7eb1faa6ebabb9f050b0f24e14b704ad66e3bb66
sharan-dev-ae <156191048+sharan-dev-ae@users.noreply.github.com>
2025-04-03 16:46:32 +0400

    Fix: Removed dynamic import

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,8 @@
 // app/home/page.tsx (Home Page Component)
-import dynamic from "next/dynamic";
 import Blog from "../components/Blog";
 import Contact from "../components/Contact";
 import Education from "../components/Education";
-// import Experience from "../components/Experience";
+import Experience from "../components/Experience";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
@@ -11,19 +10,14 @@ import Skillset from "../components/Skillset";
 import Statistics from "../components/Statistics";
 
 export default function Home() {
-  const ExperienceComponent = dynamic(
-    () => import("../components/Experience"),
-    {
-      ssr: false, // Disable SSR for this component
-    }
-  );
+  
   return (
     <div>
       <Navbar />
       <Hero />
       <Statistics />
       <Skillset />
-      <ExperienceComponent />
+      <Experience />
       <Projects />
       <Education />
       <Blog />
