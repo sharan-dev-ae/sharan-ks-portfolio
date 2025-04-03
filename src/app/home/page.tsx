89a8b8beed1864993c8ff8e84022841f3acdfe60
sharan-dev-ae <156191048+sharan-dev-ae@users.noreply.github.com>
2025-04-03 16:41:22 +0400

    Fix: Included dynamic import for experience component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,9 @@
 // app/home/page.tsx (Home Page Component)
+import dynamic from "next/dynamic";
 import Blog from "../components/Blog";
 import Contact from "../components/Contact";
 import Education from "../components/Education";
-import Experience from "../components/Experience";
+// import Experience from "../components/Experience";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
@@ -10,13 +11,19 @@ import Skillset from "../components/Skillset";
 import Statistics from "../components/Statistics";
 
 export default function Home() {
+  const ExperienceComponent = dynamic(
+    () => import("../components/Experience"),
+    {
+      ssr: false, // Disable SSR for this component
+    }
+  );
   return (
     <div>
       <Navbar />
       <Hero />
       <Statistics />
       <Skillset />
-      <Experience />
+      <ExperienceComponent />
       <Projects />
       <Education />
       <Blog />
